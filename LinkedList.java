e962ff888da5cfcdf55aed0694f2a31b3cb40b04
surajp916 <99369174+surajp916@users.noreply.github.com>
2024-09-29 18:47:04 -0400

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,25 @@
+package spiderman;
+
+public class LinkedList<T> {
+
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    public Object getFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFirst'");
+    }
+
+    public void addFirst(int d) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addFirst'");
+    }
+
+    public void add(Object first) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+}
